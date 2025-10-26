6232cd886ccb935f38f34f9404dd9ca4519356e7
Khong <hankhongg@gmail.com>
2025-10-26 19:30:34 +0700

    chore: add gen folder + automatically regen .proto file on build

--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -1,2 +1,4 @@
 export * from './lib/shared.module';
-export * from './lib/enums/shared-enums'
+export * from './lib/enums/shared-enums';
+export * as driverPackage from './lib/gen/driver';
+export * as tripPackage from './lib/gen/trip';
