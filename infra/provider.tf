6f7c1d1330048993a15ea1e87be780d630d35e20
Khong <hankhongg@gmail.com>
2025-10-29 15:55:41 +0700

    feat(infra): add variable.tf & provider.rf

---
+++ infra/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    aws = { source = "hashicorp/aws", version = "~> 5.60" }
+    random = { source = "hashicorp/random", version = "~> 3.6" }
+    rabbitmq = { source = "cyrilgdn/rabbitmq", version = "~> 1.14" }
+  }
+}
+
+provider "aws" { region = var.region }
+
+# RabbitMQ Management API (private inside VPC)
+provider "rabbitmq" {
+  endpoint = var.rabbitmq_mgmt_url
+  username = var.rabbitmq_username
+  password = var.rabbitmq_password
+}
---
+++ infra/variable.tf
@@ -0,0 +1,87 @@
+variable "project"   {
+  type = string
+  default = "uit-go"
+  }
+variable "env"       {
+  type = string
+  default = "dev"
+  }
+variable "region"    {
+  type = string
+  default = "ap-southeast-1"
+  }
+variable "vpc_cidr"  {
+  type = string
+  default = "10.20.0.0/16"
+  }
+variable "az_count"  {
+  type = number
+  default = 2
+  }
+
+# images (will input real image tags later)
+variable "container_image_map" {
+  type = map(string)
+  default = {
+    "api-gateway"  = "ghcr.io/uit-go/api-gateway:latest"
+    "auth-service" = "ghcr.io/uit-go/auth:latest"
+    "user-service" = "ghcr.io/uit-go/user:latest"
+    "trip-service" = "ghcr.io/uit-go/trip:latest"
+    "driver-service" = "ghcr.io/uit-go/driver:latest"
+    "notification-service" = "ghcr.io/uit-go/notification:latest"
+  }
+}
+
+# resources
+variable "container_cpu" {
+  type = number
+  default = 512
+  }
+variable "container_mem" {
+  type = number
+  default = 1024
+  }
+variable "desired_count" {
+  type = number
+  default = 1
+  }
+
+# DB master (for all 3 RDS instances)
+variable "db_master_username" {
+  type = string
+  default = "appadmin"
+  }
+variable "db_master_password" {
+  type = string
+  default = null
+  sensitive = true
+  }
+
+
+# Supabase (for api-gateway & auth-service)
+variable "supabase_url"       {
+  type = string
+  default = ""
+  }
+variable "supabase_key"       {
+  type = string
+  default = ""
+  sensitive = true
+  }
+variable "supabase_jwt_secret"{
+  type = string
+  default = ""
+  sensitive = true
+  }
+
+# RabbitMQ Management API
+variable "rabbitmq_mgmt_url" { type = string }
+variable "rabbitmq_username" {
+  type = string
+  default = "mqadmin"
+  }
+
+variable "rabbitmq_password" {
+  type = string
+  sensitive = true
+  }
