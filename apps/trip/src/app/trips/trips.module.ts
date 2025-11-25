7cc325b442092310ecc9174f7c5b1fccb0fb5748
Khong <hankhongg@gmail.com>
2025-11-25 14:35:16 +0700

    chore: change hardcode to env var

--- apps/trip/src/app/trips/trips.module.ts
+++ apps/trip/src/app/trips/trips.module.ts
@@ -9,44 +9,54 @@ import { tripPackage } from '@uit-go-backend/shared';
 import { join } from 'path';
 import { TripsListener } from '../rmqService/trips.listener';
 import { RoutingService } from '../routing/routing.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 // import { RmqService } from '../rmqService/rmq.service';
 
 @Module({
   imports: [
+    ConfigModule,
     TypeOrmModule.forFeature([Trip, Rating]),
-    ClientsModule.register([
+    ClientsModule.registerAsync([
       {
         name: 'DRIVER_SERVICE_RMQ',
-        transport: Transport.RMQ,
-        options: {
-          urls: ['amqp://guest:guest@rabbitmq:5672'],
-          queue: 'driver.q',
-          queueOptions: {
-            durable: true,
-            // deadLetterExchange: 'trip-dlx',
-            // messageTtl: 15000, // 15s
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get<string>('RABBITMQ_URL')],
+            queue: 'driver.q',
+            queueOptions: {
+              durable: true,
+              // deadLetterExchange: 'trip-dlx',
+              // messageTtl: 15000, // 15s
+            }
           }
-        }
+        }),
+        inject: [ConfigService],
       },
       {
         name: 'NOTIFICATION_SERVICE_RMQ',
-        transport: Transport.RMQ,
-        options: {
-          urls: ['amqp://guest:guest@rabbitmq:5672'],
-          queue: 'notif.q',
-          queueOptions: {
-            durable: true
-          }
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get<string>('RABBITMQ_URL')],
+            queue: 'notif.q',
+            queueOptions: {
+              durable: true
+            }
         }
+        }),
+        inject: [ConfigService],
       },
       {
         name: 'USER_SERVICE',
-        transport: Transport.TCP,
-        options: {
-          // host: process.env.USER_SERVICE_HOST || 'user-service',
-          host: 'user-service',
-          port: parseInt(process.env.USER_SERVICE_PORT || '3002'),
-        }
+        useFactory: () => ({
+          transport: Transport.TCP,
+          options: {
+            // host: process.env.USER_SERVICE_HOST || 'user-service',
+            host: 'user-service',
+            port: parseInt(process.env.USER_SERVICE_PORT || '3002'),
+          }
+        })
       }
     ]),
   ],
